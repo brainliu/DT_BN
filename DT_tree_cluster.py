cef5247edae3b192558fa6e3ad9a7d68e4f83cce
brainliu <406096277@qq.com>
2019-11-04 21:25:07 +0800

    2019-11-04

--- DT_tree_cluster.py
+++ DT_tree_cluster.py
@@ -3,8 +3,9 @@
 # create time :2019/11/4-21:17 
 #location: sichuan chengdu
 import math
-import numpy as numpy
+import numpy as np
 class Cluster:
     def __init__(self,x,y,sample_weight=None,base=2):
         self._x = x.T,self._y = y
-
+        if sample_weight is None:
+            self._counters=np.bincount(self._y)
